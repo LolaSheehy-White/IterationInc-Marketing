f390b8dec17558a978171f8b168f7fc11e0501ab
LolaSheehy-White <ls2062@hw.ac.uk>
2025-02-23 21:10:03 +0000

    adding help route

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Navbar from "./components/nav.js";
 import Footer from "./components/footer.js";
 import HomeScreen from "./pages/HomeScreen.js";
 import SignUp from "./pages/SignUp.js";
+import AboutUs from "./pages/AboutUs.js";
+import HelpPage from "./pages/Help.js";
 
 import "./navfoot.css";
 import "./home.css";
 import "./signup.css";
 import "./aboutus.css";
+import "./help.css";
 
 function App() {
   return (
@@ -17,6 +20,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<HomeScreen />} />
         <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/Help" element={<HelpPage />} />
         <Route path="/footer" element={<Footer />} />
       </Routes>
     </Router>
