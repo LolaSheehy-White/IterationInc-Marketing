82459e37669143fc1cd42479a4a487083a504f66
LolaSheehy-White <ls2062@hw.ac.uk>
2025-03-07 16:28:06 +0000

    more reviews

--- src/pages/reviews.js
+++ src/pages/reviews.js
@@ -12,11 +12,11 @@ const reviews = [
     { id: 1, name: 'Lorelai', rating: 5, title: 'Excellent Product', content: 'Super easy to use and implement into our care-home!', date: '2025-02-20' },
     { id: 2, name: 'Rory', rating: 4, title: 'Great Product', content: 'The product is great, but it was quite pricy for our budget', date: '2024-09-18' },
     { id: 3, name: 'Luke', rating: 5, title: 'Excellent Service', content: 'I had a fantastic experience with this company. Highly recommend!', date: '2024-10-05' },
-    { id: 4, name: 'Jess', rating: 4, title: 'Very good', content: 'The product quality is great, but I wish families could join to support as not all residents are as capeable', date: '2025-02-18' },
+    { id: 4, name: 'Jess', rating: 4, title: 'Very good', content: 'The product quality is great, but I wish families could join to support as not all residents are as capable', date: '2025-02-18' },
     { id: 5, name: 'Paris', rating: 5, title: 'Brilliant company', content: 'It is a great comfort to know my mum is being cared for not only by the carers but also by innovative companies like...', date: '2024-12-14' },
-    { id: 6, name: 'Lane', rating: 4, title: 'Great Product', content: 'The product quality is great, but shipping was a bit slow.', date: '2025-01-24' },
-    { id: 7, name: 'Dave', rating: 5, title: 'Excellent Service', content: 'I had a fantastic experience with this company. Highly recommend!', date: '2025-03-03' },
-    { id: 8, name: 'Sookie', rating: 4, title: 'Great Product', content: 'The product quality is great, but shipping was a bit slow.', date: '2025-02-01' }
+    { id: 6, name: 'Lane', rating: 4, title: 'Good', content: 'We had a couple problems initially but we contacted IterationInc and they were so helpful and made sure everything worked.', date: '2025-01-24' },
+    { id: 7, name: 'Dave', rating: 5, title: 'Love it!', content: 'All residents have managed to use this no matter their abilities and we love being able to check in on them quickly as carers!', date: '2025-03-03' },
+    { id: 8, name: 'Sookie', rating: 4, title: 'Smart product', content: 'Needs a couple adjustments for my care home but I have spoken to IterationInc and they are working on it already!', date: '2025-02-01' }
 ];
 
 // Sort reviews by rating (highest to lowest)
